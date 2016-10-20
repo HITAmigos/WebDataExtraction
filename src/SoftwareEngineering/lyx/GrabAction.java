8a1b7b843c4af0a2cee3dca350ecb72526aceee4
Eric <1486587187@qq.com>
2016-10-20 19:41:56 +0800

    后台整体框架构建

---
+++ src/SoftwareEngineering/lyx/GrabAction.java
@@ -0,0 +1,14 @@
+package SoftwareEngineering.lyx;
+
+/**
+ * 抓取表格信息.
+ * @author liuyx
+ * 从网页内容字符串中摘出出一个表格
+ */
+public class GrabAction extends TableAction {
+  @Override
+  public String execute() {
+    String result = "failure";
+    return result;
+  }
+}
