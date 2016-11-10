8cbf35a3ca76428aac03588ccbde994cb61bde7d
Eric <1486587187@qq.com>
2016-11-10 16:59:28 +0800

    增加行列修改功能

--- Up2UModuleTest/src/SoftwareEngineering/lyx/UpdateAction.java
+++ Up2UModuleTest/src/SoftwareEngineering/lyx/UpdateAction.java
@@ -3,39 +3,94 @@ package SoftwareEngineering.lyx;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * ¸üĞÂ±í¸ñ.
- * 
- * @author liuyx ½ÓÊÜÒ»¸ö±í¸ñ²¢¸üĞÂÊı¾İ¿â
- */
 public class UpdateAction extends TableAction {
 
-  private String ColName;
+  private String Tablename;
+  private int ColNum;
   private int RowNum;
+  private String[] newData;
 
-
-  public String getColName() {
-    return ColName;
+  public String getTablename() {
+    return Tablename;
   }
 
-  public void setColName(String colName) {
-    ColName = colName;
+  public void setTablename(String tablename) {
+    Tablename = tablename;
+  }
+  
+  public String[] getNewData() {
+    return newData;
+  }
+
+  public void setNewData(String[] newData) {
+    this.newData = newData;
+  }
+
+  public void setColNum(int ColNum) {
+    this.ColNum = ColNum;
+  }
+
+  public int getColNum() {
+    return ColNum;
   }
 
   public int getRowNum() {
     return RowNum;
   }
 
-  public void setRowNum(int rowNum) {
-    RowNum = rowNum;
+  public void setRowNum(int RowNum) {
+    this.RowNum = RowNum;
   }
 
-  public String Update() {
+  /**
+   * æ›´æ–°åˆ—æ•°æ®.
+   * @return
+   */
+  public String UpdateCol() {
     String result = "failure";
     DBConnection dbHelper = new DBConnection();
     List<ChangeUnit> change = new ArrayList<ChangeUnit>();
 
-    if (dbHelper.Update(Username,change)) {
+    for(int i = 0 ; i < newData.length ; i++ ){
+      if(newData[i]!=null){
+        ChangeUnit temp = new ChangeUnit();
+        int[] pos = new int[2];
+        pos[0] = ColNum+3;
+        pos[1] = i+2;
+        temp.setChange(newData[i]);
+        temp.setPosition(pos);
+        change.add(temp);
+      }
+    }
+    
+    if (dbHelper.Update(Tablename,change)) {
+      result = "success";
+    }
+    return result;
+  }
+  
+  /**
+   * æ›´æ–°è¡Œæ•°æ®.
+   * @return
+   */
+  public String UpdateRow() {
+    String result = "failure";
+    DBConnection dbHelper = new DBConnection();
+    List<ChangeUnit> change = new ArrayList<ChangeUnit>();
+
+    for(int i = 0 ; i < newData.length ; i++ ){
+      if(newData[i]!=null){
+        ChangeUnit temp = new ChangeUnit();
+        int[] pos = new int[2];
+        pos[0] = RowNum+2;
+        pos[1] = i+1;
+        temp.setChange(newData[i]);
+        temp.setPosition(pos);
+        change.add(temp);
+      }
+    }
+    
+    if (dbHelper.Update(Tablename,change)) {
       result = "success";
     }
     return result;
@@ -44,7 +99,7 @@ public class UpdateAction extends TableAction {
   public String DeleteCol() {
     String result = "failure";
     DBConnection dbHelper = new DBConnection();
-    if(dbHelper.DeleteCol(Username,ColName)){
+    if(dbHelper.DeleteCol(Username,ColNum)){
       result = "success";
     }
     return result;
@@ -59,8 +114,26 @@ public class UpdateAction extends TableAction {
     return result;
   }
   
-  public void main(String args[]){
+  public static void main(String args[]){
     UpdateAction ua = new UpdateAction();
-    ua.DeleteRow();
+    
+//    String[] updateRow = new String[11];
+//    for(int i = 0 ; i < updateRow.length ; i++){
+//      updateRow[i] = null;
+//    }
+//    updateRow[0] = "description";
+//    updateRow[10] = "define column's group";
+//    ua.setTablename("lyx-39");
+//    ua.setColNum(2);
+//    ua.setNewData(updateRow);
+//    ua.UpdateCol();
+  
+//    String[] updateCol = new String[2];
+//    updateCol[0] = "aaa";
+//    updateCol[1] = "bbb";
+//    ua.setTablename("lyx-39");
+//    ua.setRowNum(2);
+//    ua.setNewData(updateCol);
+//    ua.UpdateCol();
   }
 }
