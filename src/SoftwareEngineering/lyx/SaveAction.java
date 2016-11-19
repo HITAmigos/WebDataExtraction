86deb0bd7284c328aad959d0eb5fdde8d32e84b7
匡盟盟 <kuangmeng@msn.com>
2016-11-19 13:47:53 +0800

    整理项目

--- src/SoftwareEngineering/lyx/SaveAction.java
+++
@@ -1,9 +0,0 @@
-package SoftwareEngineering.lyx;
-
-public class SaveAction extends TableAction {
-  @Override
-  public String execute() {
-    String result = "failure";
-    return result;
-  }
-}
