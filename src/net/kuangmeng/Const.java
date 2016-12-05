9822209c5841833c170b59ba10754c299b3c1ebb
匡盟盟 <kuangmeng@msn.com>
2016-12-05 14:55:03 +0800

    调整

--- src/net/kuangmeng/Const.java
+++ src/net/kuangmeng/Const.java
@@ -1,8 +1,8 @@
 package net.kuangmeng;
 public class Const {
-	public static final String DB_URL = "jdbc:mysql://localhost:3306/graphgrabtest?useUnicode=true&characterEncoding=utf8&&useSSL=false";
+	public static final String DB_URL = "jdbc:mysql://localhost:3306/up2u?useUnicode=true&characterEncoding=utf8&&useSSL=false";
     public static final String USER = "root";
-    public static final String PASS = "123456789";
+    public static final String PASS = "601939";
     private int id=0;
     public String getDB_URL(){
    	 return DB_URL;
