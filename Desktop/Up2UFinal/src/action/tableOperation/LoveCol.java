ce43ce627dabefa2dca5c4aecde2770e96be0996
reecefan <2284789237@qq.com>
2016-12-05 22:22:22 +0800

    调整tableOperation类

--- Desktop/Up2UFinal/src/action/tableOperation/LoveCol.java
+++ Desktop/Up2UFinal/src/action/tableOperation/LoveCol.java
@@ -1,6 +1,9 @@
 package action.tableOperation;
 
+import java.util.ArrayList;
+
 import action.Action;
+import entity.Database;
 
 public class LoveCol extends Action {
   private int colNum = 0;
@@ -17,7 +20,27 @@ public class LoveCol extends Action {
   //将数据库中对应表格列的一行中"0X"->"1X"
   @Override
   public String execute() {
-    return "failure";
+    String result = "success";
+    char[] tag = null;
+    ArrayList<String> columnName = new ArrayList<String>();
+    ArrayList<Object> value = new ArrayList<Object>();
+    columnName.add("id");
+    value.add(new Integer(1));
+    Database db = new Database(tablename);
+    tag = db.getRecord("id",1)[colNum+1].toCharArray();
+    tag[1] = '1';
+    columnName.add(new Integer(colNum).toString());
+    value.add(new String(tag));
+    
+    if(!db.update(columnName, value)){
+     result = "failure";
+    }
+    return result;
   }
-
+  public static void main(String args[]){
+    LoveCol lc = new LoveCol();
+    lc.setTablename("lyx-5");
+    lc.setColNum(2);
+    lc.execute();
+  }
 }
