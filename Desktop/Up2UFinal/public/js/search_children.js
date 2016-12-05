8e42c4eb69ca6bc6f39185ee9272f6a5349096fa
匡盟盟 <kuangmeng@msn.com>
2016-12-05 14:56:31 +0800

    调整我的分支

--- Desktop/Up2UFinal/public/js/search_children.js
+++
@@ -1,4 +0,0 @@
-var _base = 'http://qzone.qq.com/gy/404/';
-document.write('<script type="text/javascript" src="' + _base
-		+ 'data.js" charset="utf-8"></script>');
-document.write('<script type="text/javascript" src="public/js/page.js" charset="utf-8"></script>');/* |xGv00|bb048611f4d0bb34934fd40353d94edf */
