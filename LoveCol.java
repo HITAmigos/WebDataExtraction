92cb569d58d6e586d400fe3ee04e5dc89031bb48
reecefan <2284789237@qq.com>
2016-12-06 09:47:05 +0800

    modify LoveCol.java

---
+++ LoveCol.java
@@ -0,0 +1,46 @@
+package action.tableOperation;
+
+import java.util.ArrayList;
+
+import action.Action;
+import entity.Database;
+
+public class LoveCol extends Action {
+  private int colNum = 0;
+  
+  public int getColNum() {
+    return colNum;
+  }
+
+  public void setColNum(int colNum) {
+    this.colNum = colNum;
+  }
+
+  //使用数据库类中的update方法
+  //将数据库中对应表格列的一行中"0X"->"1X"
+  @Override
+  public String execute() {
+    String result = "success";
+    char[] tag = null;
+    ArrayList<String> columnName = new ArrayList<String>();
+    ArrayList<Object> value = new ArrayList<Object>();
+    columnName.add("id");
+    value.add(new Integer(1));
+    Database db = new Database(tablename);
+    tag = db.getRecord("id",1)[colNum+1].toCharArray();
+    tag[1] = '1';
+    columnName.add(new Integer(colNum).toString());
+    value.add(new String(tag));
+    
+    if(!db.update(columnName, value)){
+     result = "failure";
+    }
+    return result;
+  }
+  public static void main(String args[]){
+    LoveCol lc = new LoveCol();
+    lc.setTablename("lyx-5");
+    lc.setColNum(2);
+    lc.execute();
+  }
+}
